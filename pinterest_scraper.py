6d774d5a8bacfc4ccf540e07c0fb4d9c57f1243c
julianev <juliane.verwiebe@jevernet.de>
2024-12-02 12:47:23 +0100

    update chromedriver and change query

--- pinterest_scraper.py
+++ pinterest_scraper.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from uuid import uuid4
 
--- test_pinterest_scraper.py
+++ test_pinterest_scraper.py
@@ -1,4 +1,9 @@
 from pinterest_scraper import PinterestScraper
 
-pattern_scraper = PinterestScraper("abstract pattern")
+# Define query for searching images
+pattern_scraper = PinterestScraper("wavy pattern")
 pattern_scraper.run()
+
+# or scrape your own pinterest board
+#pattern_scraper = PinterestScraper("user_name","board_name")
+#pattern_scraper.run()
